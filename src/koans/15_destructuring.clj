0c61211fa5f740c6ee916dd868ad403d5e329c1d
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2022-04-27 15:32:10 +0100

    answers

--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -1,5 +1,6 @@
 (ns koans.15-destructuring
-  (:require [koan-engine.core :refer :all]))
+  (:require [koan-engine.core :refer :all]
+            [clojure.string :as string]))
 
 (def test-address
   {:street-address "123 Test Lane"
@@ -7,38 +8,40 @@
    :state "TX"})
 
 (meditations
-  "Destructuring is an arbiter: it breaks up arguments"
-  (= __ ((fn [[a b]] (str b a))
-         [:foo :bar]))
+ "Destructuring is an arbiter: it breaks up arguments"
+ (= ":bar:foo" ((fn [[a b]] (str b a))
+                [:foo :bar]))
 
-  "Whether in function definitions"
-  (= (str "An Oxford comma list of apples, "
-          "oranges, "
-          "and pears.")
-     ((fn [[a b c]] __)
-      ["apples" "oranges" "pears"]))
+ "Whether in function definitions"
+ (= (str "An Oxford comma list of apples, "
+         "oranges, "
+         "and pears.")
+    ((fn [[a b c]] (format "An Oxford comma list of %s, %s, and %s." a b c))
+     ["apples" "oranges" "pears"]))
 
-  "Or in let expressions"
-  (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
-     (let [[first-name last-name & aliases]
-           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
-       __))
+ "Or in let expressions"
+ (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
+    (let [[first-name last-name & aliases]
+          (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
+      (format "%s %s%s" first-name last-name (apply str (map #(str " aka " %) aliases)))))
 
-  "You can regain the full argument if you like arguing"
-  (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
-     (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+ "You can regain the full argument if you like arguing"
+ (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
+    (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
+      {:original-parts full-name :named-parts {:first first-name :last last-name}}))
 
-  "Break up maps by key"
-  (= "123 Test Lane, Testerville, TX"
-     (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+ "Break up maps by key"
+ (= "123 Test Lane, Testerville, TX"
+    (let [{street-address :street-address, city :city, state :state} test-address]
+      (string/join ", " [street-address, city, state])))
 
-  "Or more succinctly"
-  (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+ "Or more succinctly"
+ (= "123 Test Lane, Testerville, TX"
+    (let [{:keys [street-address city state]} test-address]
+      (string/join ", " [street-address, city, state])))
 
-  "All together now!"
-  (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+ "All together now!"
+ (= "Test Testerson, 123 Test Lane, Testerville, TX"
+    ((fn [[first-name last-name] {:keys [street-address city state]}]
+       (str first-name " " last-name ", " (string/join ", " [street-address, city, state])))
+     ["Test" "Testerson"] test-address)))
